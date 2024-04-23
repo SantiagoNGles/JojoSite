7cef06066031077b45b215ea7929ee900a62ed7a
Santiago <119554381+SantiagoNGles@users.noreply.github.com>
2024-04-23 11:13:49 +0200

    Delete dossJojoTest directory

--- dossJojoTest/controleur/mentions.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
-    $racine="..";
-}
-
-// appel du script de vue qui permet de gerer l'affichage des donnees
-$titre = "Mentions légales";
-include "$racine/vue/entete2.html.php";
-include "$racine/vue/vueMentions.php";
--- dossJojoTest/vue/pied2.html.php
+++
@@ -1,9 +0,0 @@
-<footer>
-
-    <a href="./?action=mentions" alt="Ce lien mène vers les mentions légales">Mentions légales</a>
-
-</footer>
-
-</body>
-
-</html>
