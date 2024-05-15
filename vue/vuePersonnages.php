1a87405b01d2c514d7358b9451d1e6aa37f36acb
unknown <snaranjograjales@gmail.com>
2024-05-15 12:44:54 +0200

    feat(restriction): add restriction of access CRUD for logged-in users

--- vue/vuePersonnages.php
+++ vue/vuePersonnages.php
@@ -23,52 +23,62 @@
                 <tbody>
                     <?php foreach ($personnages as $personnage): ?>
                         <tr>
+                            
                             <td><?php echo $personnage['idPerso']; ?></td>
                             <td><?php echo $personnage['prenomPerso']; ?></td>
                             <td><?php echo $personnage['nomPerso']; ?></td>
                             <td><?php echo $personnage['idPartie']; ?></td>
-                            <td>
-                                <form action="./?action=modifierPersonnage" method="post">
-                                    <input type="hidden" name="id" value="<?php echo $personnage['idPerso']; ?>">
-                                    <label for="prenom">Nouveau prénom:</label>
-                                    <input type="text" id="prenom" name="prenom" value="<?php echo $personnage['prenomPerso']; ?>">
-                                    <label for="nom">Nouveau nom:</label>
-                                    <input type="text" id="nom" name="nom" value="<?php echo $personnage['nomPerso']; ?>">
-                                    <input type="submit" value="Modifier">
-                                </form>
-                                <form action="./?action=supprimerPersonnage" method="post">
-                                    <input type="hidden" name="id" value="<?php echo $personnage['idPerso']; ?>">
-                                    <input type="submit" value="Supprimer">
-                                </form>
-                            </td>
+
+                            <?php if (isLoggedOn()): ?>
+                                <td>
+                                    <form action="./?action=modifierPersonnage" method="post">
+                                        <input type="hidden" name="id" value="<?php echo $personnage['idPerso']; ?>">
+                                        <label for="prenom">Nouveau prénom:</label>
+                                        <input type="text" id="prenom" name="prenom" value="<?php echo $personnage['prenomPerso']; ?>">
+                                        <label for="nom">Nouveau nom:</label>
+                                        <input type="text" id="nom" name="nom" value="<?php echo $personnage['nomPerso']; ?>">
+                                        <input type="submit" value="Modifier">
+                                    </form>
+                                    <form action="./?action=supprimerPersonnage" method="post">
+                                        <input type="hidden" name="id" value="<?php echo $personnage['idPerso']; ?>">
+                                        <input type="submit" value="Supprimer">
+                                    </form>
+                                </td>
+                            <?php endif; ?>
+
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
+            
             </table>
 
-            <form action="./?action=ajouterPersonnage" method="post">
+                <?php if(isLoggedOn()): ?>
 
-                <label for="prenom">Prénom:</label>
-                <input type="text" id="prenom" name="prenom">
-                <label for="nom">Nom:</label>
-                <input type="text" id="nom" name="nom">
+                <form action="./?action=ajouterPersonnage" method="post">
 
-                <label for="idPartie">Id Partie:</label>
-                <select id="idPartie" name="idPartie">
-                    <option value="1">Partie 1</option>
-                    <option value="2">Partie 2</option>
-                    <option value="3">Partie 3</option>
-                    <option value="4">Partie 4</option>
-                    <option value="5">Partie 5</option>
-                    <option value="6">Partie 6</option>
-                </select>
+                    <label for="prenom">Prénom:</label>
+                    <input type="text" id="prenom" name="prenom">
+                    <label for="nom">Nom:</label>
+                    <input type="text" id="nom" name="nom">
+
+                    <label for="idPartie">Id Partie:</label>
+                    <select id="idPartie" name="idPartie">
+                        <option value="1">Partie 1</option>
+                        <option value="2">Partie 2</option>
+                        <option value="3">Partie 3</option>
+                        <option value="4">Partie 4</option>
+                        <option value="5">Partie 5</option>
+                        <option value="6">Partie 6</option>
+                    </select>
                 
-                <input type="submit" value="Ajouter">
+                    <input type="submit" value="Ajouter">
 
-            </form>
+                </form>
+
+                <?php endif; ?>
 
         </div>
-
+        
     </article>
 
 </main>
